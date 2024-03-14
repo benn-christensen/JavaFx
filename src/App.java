e370db1657a076c5bbac21e8c8f8775ed414a5b7
Benn Christensen <bnch@eaaa.dk>
2024-03-14 14:21:37 +0100

    Initial commit

--- src/App.java
+++ src/App.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class app {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("Hello, World!");
+    }
 }
